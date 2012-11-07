49c0c7685d2a0f430c0598d40aefece38c4d99c0
Eneko Taberna <eneko.taberna@gmail.com>
2012-11-07 20:35:46 +0100

    Some minor improvements

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -1,9 +1,16 @@
 class TipsController < InheritedResources::Base
-  set_tab :tips
+  set_tab :home
   before_filter :authenticate_user!, only: [:create, :update]
+  before_filter :set_new_tab, only: :new
 
   def create
     @tip = current_user.tips.new params[:tip]
     create!
+  end
+
+  private
+
+  def set_new_tab
+    set_tab [:new]
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,11 @@
 @user = User.create email: "user@dailytip.com", password: "aaaaaa", password_confirmation: "aaaaaa"
+
+25.times do |index|
+  tip = @user.tips.create title: Faker::Lorem.sentence,
+                       content: Faker::Lorem.paragraph(2),
+                       tag_list: "tag#{rand(5)},tag#{rand(5)}"
+  5.times do |t|
+    tip.links.create url: Faker::Internet.url,
+                     description: Faker::Lorem.sentence
+  end
+end
