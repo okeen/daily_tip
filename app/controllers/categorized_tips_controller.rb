8d6407fee8149636f6b514054bfed5852d42efee
Eneko Taberna <eneko.taberna@gmail.com>
2012-11-13 01:08:25 +0100

    Scope tag cloud to category

--- app/controllers/categorized_tips_controller.rb
+++ app/controllers/categorized_tips_controller.rb
@@ -2,6 +2,7 @@ class CategorizedTipsController < TipsController
   defaults :resource_class => Tip, :collection_name => 'tips', :instance_name => 'tip'
 
   before_filter :load_category
+  before_filter :load_tags, only: [:index, :tagged, :popular, :categorized]
 
   protected
 
@@ -11,5 +12,9 @@ class CategorizedTipsController < TipsController
 
   def load_category
     @current_category = Category.find_by_name request.subdomain
+  end
+
+  def load_tags
+    @tags = @current_category.tips.tag_counts_on :tags
   end
 end
